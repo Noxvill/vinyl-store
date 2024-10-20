92c4b0fe799ad0faa42e830ecf3807e8948d059b
Noxvill <gmo_mmo@live.cl>
2024-10-20 18:56:09 -0300

    CAMBIOS ENV

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/routes');
+require('dotenv').config()
 const { authenticateToken } = require('./middlewares/auth');
 const loggerManager = require('./middlewares/loggerManager');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+
+    origin:[process.env.CLIENT_URL]
+
+
+}));
 app.use(loggerManager);
 app.use('/api', routes);
 
