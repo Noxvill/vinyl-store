4edc798b369b957cbd2d980d5f0d724c313a7885
Noxvill <gmo_mmo@live.cl>
2024-10-21 03:08:00 -0300

    create user

--- frontend/src/components/Context/GlobalContext.jsx
+++ frontend/src/components/Context/GlobalContext.jsx
@@ -272,12 +272,44 @@ export const GlobalProvider = ({ children }) => {
     }
   };
 
+
+  const registerUser = async (username, mail, password, rol, ubicacion) => {
+        try {
+          const response = await fetch('http://localhost:3000/api/users/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ nombre: username, mail, contraseña: password, rol, ubicacion }),
+          });
+      
+          // Verificar si la respuesta es exitosa
+          if (!response.ok) {
+            const errorData = await response.json(); // Intentar obtener el error del cuerpo de la respuesta
+            throw new Error(errorData.message || 'Error al registrar el usuario');
+          }
+      
+          // Verificar que la respuesta tenga contenido JSON
+          const contentLength = response.headers.get('content-length');
+          if (contentLength && contentLength !== '0') {
+            const newUser = await response.json();
+            setUser(newUser); // Guarda el nuevo usuario en el estado
+          } else {
+            console.log('Registro exitoso sin respuesta en el cuerpo');
+          }
+        } catch (err) {
+          console.error('Error:', err.message);
+          setError(err.message); // Maneja el error en el frontend
+        }
+      };
+    
+
   useEffect(() => {
     fetchProducts();
   }, []);
 
   return (
-    <GlobalContext.Provider value={{ user, login, logout, token, createProduct, cart, setCart, products, loading, error }}>
+    <GlobalContext.Provider value={{ user, registerUser, login, logout, token, createProduct, cart, setCart, products, loading, error }}>
       {children}
     </GlobalContext.Provider>
   );
